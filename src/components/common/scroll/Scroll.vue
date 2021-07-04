c61b12ba22da88ffc5a3b09bb6baee9342953e25
xiaolong dong <249871094@qq.com>
2021-07-05 00:42:29 +0800

    '首页‘

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+
+</template>
+
+<script>
+// import BScroll from '@better-scroll/core'
+import BScroll from '@better-scroll/core'
+
+export default {
+  name: "Scroll",
+  data(){
+    return {
+      scroll:null
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper,{
+      observeDom:true,
+      observeImage:true
+    })
+    console.log(this.scroll);
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fea">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeatyreView"
+}
+</script>
+
+<style scoped>
+
+</style>
